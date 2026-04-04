5e0e1ba7ddd0311c3f656876b7d4905a07395ec6
Evgeniy <krionic1@gmail.com>
2026-04-04 16:32:27 +0300

    Добавляет генерацию данных

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,4 @@
-const validateLength = (text, maxLength) => {
-  if (text.length > maxLength) {
-    return false;
-  }
-  return true;
-};
+const validateLength = (text, maxLength) => text.length <= maxLength;
 
 const isPalindrome = (text) => {
   const normalizedText = text.toLowerCase().replaceAll(' ', '');
@@ -12,31 +7,10 @@ const isPalindrome = (text) => {
 };
 
 const extractDigits = (text) => {
+  let tmpText = text;
   if (typeof text === 'number') {
-    text = text.toString();
+    tmpText = text.toString();
   }
-  return parseInt(text.replace(/\D+/g, ''), 10);
+  return parseInt(tmpText.replace(/\D+/g, ''), 10);
 };
-
-
-console.warn('Проверка первой функции');
-console.log(validateLength('проверяемая строка', 20));
-console.log(validateLength('проверяемая строка', 18));
-console.log(validateLength('проверяемая строка', 10));
-
-console.warn('Проверка второй функции');
-console.log(isPalindrome('топот'));
-console.log(isPalindrome('ДовОд'));
-console.log(isPalindrome('Кекс'));
-console.log(isPalindrome('Лёша на полке клопа нашёл '));
-
-console.warn('Проверка третей функции');
-console.log(extractDigits('2023 год'));
-console.log(extractDigits('ECMAScript 2022'));
-console.log(extractDigits('1 кефир, 0.5 батона'));
-console.log(extractDigits('агент 007'));
-console.log(extractDigits('а я томат'));
-console.log(extractDigits(2023));
-console.log(extractDigits(-1));
-console.log(extractDigits(1.5));
 
